5f0b613e8061db2f5825a0fa299d6958d41b054f
yellowfromseegg <95318044+yellowfromseegg@users.noreply.github.com>
2026-01-26 23:48:54 +0100

    Add dark mode setup in start_fs25.sh
    Added command to set web dark mode after starting the server.

--- run/nobody/start_fs25.sh
+++ run/nobody/start_fs25.sh
@@ -8,6 +8,7 @@ export WINEPREFIX=~/.fs25server
 if [ -f ~/.fs25server/drive_c/Program\ Files\ \(x86\)/Farming\ Simulator\ 2025/dedicatedServer.exe ]
 then
     wine ~/.fs25server/drive_c/Program\ Files\ \(x86\)/Farming\ Simulator\ 2025/dedicatedServer.exe & sleep 1 && firefox "http://"$CONTAINER_IP":7999/index.html?lang=en&username="$WEB_USERNAME"&password="$WEB_PASSWORD"&login=Login"
+    bash /usr/local/bin/set-web-darkmode.sh
 else
     echo "Game not installed?" && exit
 fi
